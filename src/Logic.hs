2981274d9b04dcd1f5dd833821138fb7e3d3b3e6
FrostFlame <serazetdinow.damir@yandex.ru>
2018-10-25 14:51:15 +0300

    turn logic added

---
+++ src/Logic.hs
@@ -0,0 +1,54 @@
+markShot :: Field -> Int -> Int -> Field
+markShot field x y = replace x field (replace y (select x field) True)
+
+
+removeDestroyedShips :: [Ship] -> [Ship]
+removeDestroyedShips [] = []
+removeDestroyedShips (x:xs) | null x    = removeDestroyedShips xs
+                            | otherwise = x : removeDestroyedShips xs
+
+
+checkShipDestroyed :: Field -> Ship -> Coordinate -> (Ship, Bool)
+checkShipDestroyed field ship coordinate = if or [coordinate == coord | coord <- ship] == False then do
+                                               (ship, False)    -- Miss
+                                           else do
+                                               if and [select (fst coord) (select (snd coord) field) == True | coord <- ship, coord /= coordinate] == False then
+                                                   (ship, True) -- Hit, but not sunk
+                                               else
+                                                   ([], True)   -- Hit and sunk
+
+
+fire :: (Field, [Ship]) -> Coordinate -> (Field, [Ship], Bool)
+fire (enemyField, enemyShips) coordinate = (markShot enemyField (snd coordinate) (fst coordinate),
+                                            removeDestroyedShips [fst (checkShipDestroyed enemyField ship coordinate) | ship <- enemyShips],
+                                            or [snd (checkShipDestroyed enemyField ship coordinate) | ship <- enemyShips])
+
+
+fireWithEveryShip :: (Field, [Ship], String) -> IO (Field, [Ship])
+fireWithEveryShip (enemyField, enemyShips, name) = do
+                                                        putStrLn ("Enter the coordinates to fire shot")
+                                                        string <- getLine
+                                                        let coord = convertStringToCoordinates string
+                                                        if validateCoordinate coord then
+                                                            do
+                                                              let (newEnemyField, newEnemyShips, hit) = fire (enemyField, enemyShips) coord
+
+                                                              if hit then
+                                                                  putStrLn ("Firing at coordinate (" ++ show ((fst coord) - 1) ++ "," ++ show ((snd coord) - 1) ++ "), Hit")
+                                                              else
+                                                                  putStrLn ("Firing at coordinate (" ++ show ((fst coord) - 1) ++ "," ++ show ((snd coord) - 1) ++ "), Miss")
+
+                                                              if length newEnemyShips < length enemyShips then
+                                                                  do
+                                                                    putStrLn "You sunk my battleship!"
+                                                                    return (enemyField, enemyShips)
+                                                              else
+                                                                  return (enemyField, enemyShips)
+                                                              if hit then
+                                                                  do
+                                                                    printField name newEnemyField newEnemyShips
+                                                                    fireWithEveryShip (newEnemyField, newEnemyShips, name)
+                                                              else
+                                                                  return (enemyField, enemyShips)
+                                                        else
+                                                            return (enemyField, enemyShips)
